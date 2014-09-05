c996c9084b3c5bcd24bb7515c5c03a0ca9741a24
Seth Broweleit <hello[@]getsetbro[.]com>
2014-09-05 17:13:56 -0500

    v1

---
+++ app/app.js
@@ -0,0 +1,16 @@
+/// <reference path="//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js" />
+
+var app = angular.module('ptoPlanner', []);
+
+app.run(function () {
+    Storage.prototype.setObject = function (key, value) {
+        this.setItem(key, JSON.stringify(value));
+    };
+
+    Storage.prototype.getObject = function (key) {
+        var value = this.getItem(key);
+        return value && JSON.parse(value);
+    };
+
+    $("input.date").datepicker();
+});
