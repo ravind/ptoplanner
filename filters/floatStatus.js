8a6dc3936edbb0535851a8c7245001642a1560a5
Seth Broweleit <hello[@]getsetbro[.]com>
2014-09-12 16:23:48 -0500

    =]

--- filters/floatStatus.js
+++ filters/floatStatus.js
@@ -4,23 +4,29 @@ app.filter('floatStatus', function (ptoManager, $rootScope) {
   // function that's invoked each time Angular runs $digest()
   // pass in `item` which is the single Object we'll manipulate
   return function (item) {
+    var q = item.split('q')[1];
     var floatsList = ptoManager.getFloats();
-    if( !floatsList[item] ){
-       return ':?';
+    //no data for quarters passed
+    if( !floatsList[item] && q < $rootScope.curQuarter){
+       return '=(';
+    }
+    //no data
+    if( !floatsList || !floatsList[item] ){
+       return '=|';
     }
     //if used good job
     if( floatsList[item].used === true ){
-       return ':)';
+       return '=)';
     }
-    var q = item.split('q')[1];
     //if unused and quarter passed
     if( q < $rootScope.curQuarter ){
-      return ':(';
+      return '=(';
     }
     //if unused and quarter is not yet
     if( q > $rootScope.curQuarter ){
-      return ':|';
+      return '=|';
     }
+    //current quarter unused
     return '=/';
 
   };
