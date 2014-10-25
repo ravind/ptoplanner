31bfd4d720252b902649e52db0c00978015e674a
ravind <ravind@hotmail.com>
2014-10-24 20:45:34 -0400

    Changed default home page to Version1

--- PtoPlanner.WebService/App_Start/RouteConfig.cs
+++ PtoPlanner.WebService/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PtoPlanner.WebService
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Version1", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
